22af457b0af7598899532289db33bd5353cb3d49
Leslie Alldridge <leslie.alldridge@gmail.com>
2019-03-03 09:47:28 +1300

    Add files via upload

---
+++ Index.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Click the link below to get the SRE book from Google
+
+# This repo contains my notes on this book and a link to [where you can purchase the book from Google](https://landing.google.com/sre/books/).
+# 
+
+# 
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
